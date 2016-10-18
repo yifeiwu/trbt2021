68136250863bf8cbacd536ca3631e4077eab9802
yifeiwu <yiferic@gmail.com>
2016-10-19 08:58:55 +1200

    Handle file input

--- lib/file_reader.rb
+++ lib/file_reader.rb
@@ -1,6 +1,12 @@
 require 'robot'
 
 class FileReader
+	def initialize(filename=nil)
+	  raise ArgumentError, "file doesn't exist" unless File.file?(filename)
+	  bot = Robot.new()
+	  File.open(filename).each do |line|
+  	  end
+	end
 
 
 
--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -3,6 +3,11 @@ require './lib/file_reader'
 RSpec.describe FileReader do
   describe "Input" do
     it "opens a file" do
+      reader = FileReader.new('spec/test.txt')
+      expect(reader).to be_a FileReader
+    end
+    it "doesn't open a nonexistent file" do
+       expect { FileReader.new('blah.txt') }.to raise_error(ArgumentError)
     end
   end
 

f273c55d3cdb5ce1cb3dc8d39dbf4c12187fca1c
yifeiwu <yiferic@gmail.com>
2016-10-18 23:42:04 +1200

    Invalid placments

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,4 +1,45 @@
 class Robot
 
+  attr_reader :position, :facing
+
+  def initialize(x=nil,y=nil,f=nil)
+  	self.position = x,y
+  	self.facing = f
+  end
+
+  def position=(new_position)
+  	@position = new_position
+  end
+
+  def facing=(new_facing)
+  	@facing = new_facing
+  end
+
+  def report
+  	unless invalid_position
+  		[@position[0], @position[1], @facing]
+  	end
+  end
+
+
+
+  private
+
+  def invalid_position
+  	return true if blank_fields 
+  	return true if invalid_bounds
+  	return true if invalid_facings
+  	false
+  end
+
+  def xyf
+  	[@position[0], @position[1], @facing]
+  end
+
+  def blank_fields
+  	xyf.any?{|field| (field.nil?)} || xyf.any?{|field| field == ''}
+
+  end
+
 
 end
--- spec/robot_spec.rb
+++ spec/robot_spec.rb
@@ -1,7 +1,23 @@
 require './lib/robot'
 
 RSpec.describe Robot do
+  describe "Place and report" do
+    it "places the robot and reports its position" do
+      bot = Robot.new(1,1,'north')
+      correct_response=[1,1,'north']
+      test_response = bot.report
+      expect(test_response).to eq(correct_response)
+    end
+  end
 
+  describe "Faulty initialization" do
+    it "doesn't properly place the robot" do
+      bot = Robot.new(1,1,'')
+      correct_response=nil
+      test_response = bot.report
+      expect(test_response).to eq(correct_response)
+    end
+  end
 
 
 
